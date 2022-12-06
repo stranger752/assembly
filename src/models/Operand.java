3ea05d1194b489bb2a998afc3584c898771f64de
stranger <jsmendieta@protonmail.com>
2022-12-06 15:28:21 -0600

    se agregó interfaz Operand

---
+++ src/models/Operand.java
@@ -0,0 +1,35 @@
+package models;
+
+public interface Operand {
+
+    /**
+     * Asigna el tipo de operando, entre REG, SREG, memory o immediate.
+     */
+    void setOperandType();
+
+    /**
+     * Asiga el número de bytes que usa el operando.
+     */
+    void setNumberOfBytes();
+
+    /**
+     * Asiga el tamaño al operando, entre word o byte.
+     */
+    void setSize();
+
+    /**
+     * @return String: tipo de operando (REG, SREG, memory o immediate).
+     */
+    String getOperandType();
+
+    /**
+     * @return int: numero de bytes que usa el operando.
+     */
+    int getNumberOfBytes();
+
+    /**
+     * @return String: tipo de dato según tamaño (byte o word).
+     */
+    String getSize();
+
+}
