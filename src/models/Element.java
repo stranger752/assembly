51d6b642ca305dee076f92c6d6650f4ad69b6387
stranger <jsmendieta@protonmail.com>
2022-12-06 15:22:35 -0600

    actualización de clase Element para uso de herencia

--- src/models/Element.java
+++ src/models/Element.java
@@ -1,38 +1,47 @@
 package models;
 
 public class Element {
+    private String text;            // almacena cadena con el texto del elemento
+    private boolean valid;          // almacena TRUE si el elemento es válido, o FALSE en caso contrario
+    private String type;            // almacena tipo de pseudoinst ( o de inst o de simbolo o de cte o de reg)
 
-    private String text;
-    private String type;
-    private boolean isValid;
-
-
-    public Element(String text){
+    /**
+     * Constructor: crea elemento. Asigna el texto y valor de validez (TRUE o FALSE).
+     * @param text   String:  cadena de texto del elemento.
+     * @param valid  boolean: validez del elemento.
+     */
+    public Element(String text, boolean valid){
         this.text = text;
-        this.isValid = true;
+        this.valid = valid;
     }
 
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public String getType() {
-        return type;
-    }
-
+    /**
+     * Asigna el tipo de elemento.
+     * @param type String: tipo del elemento.
+     */
     public void setType(String type) {
         this.type = type;
     }
 
-    public boolean isValid() {
-        return isValid;
+    /**
+     * @return String: cadena de texto del elemento.
+     */
+    public String getText() {
+        return text;
     }
 
-    public void setValid(boolean valid) {
-        isValid = valid;
+    /**
+     * @return boolean: TRUE si el elemento es válido.
+     */
+    public boolean isValid() {
+        return valid;
     }
+
+    /**
+     * @return String: tipo del elemento.
+     */
+    public String getType() {
+        return type;
+    }
+
 }
