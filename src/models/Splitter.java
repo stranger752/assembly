5eb622f5cf6734aeed9d7a8cc78b253c78594c38
stranger <jsmendieta@protonmail.com>
2022-12-07 19:01:15 -0600

    creación de clase Splitter

---
+++ src/models/Splitter.java
@@ -0,0 +1,27 @@
+package models;
+
+public class Splitter {
+
+    /**
+     * Remueve espacios (" ") o tabs ("\t") al final de la cadena de texto dada como parámetro.
+     * @param line String: texto.
+     * @return String: texto sin espacios (" ") o tabs ("\t") al final.
+     */
+    public String removeEndingSpaces(String line){
+        if ( !(line.endsWith(" ") || line.endsWith("\t")) )
+            return line;
+        else return removeEndingSpaces(line.substring(0,line.length()-1));
+    }
+
+    /**
+     * Remueve espacios (" ") o tabs ("\t") al inicio de la cadena de texto dada como parámetro.
+     * @param line String: texto.
+     * @return String: texto sin espacios (" ") o tabs ("\t") al inicio de la cadena.
+     */
+    public String removeStartingSpaces(String line){
+        if ( !(line.startsWith(" ") || line.startsWith("\t")) )
+            return line;
+        else return removeStartingSpaces(line.substring(1));
+    }
+
+}
